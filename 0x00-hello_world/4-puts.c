302a6ad9ee2b28faf5f56adcff439b7cd626c0ed
Eagsor <u20075655303@gmail.com>
2022-09-09 16:26:51 -0700

    Write a C program that prints exactly "Programming is like building a multilingual puzzle, followed by a new line

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - print "*Proramming is like building a multilingual puzzle*"
+ *
+ * Return 0: if Successful
+ */
+int main(void)
+{
+
+printf("with proper grammar,but the outcome is a piece of art, \n");
+	return (0);
+}
