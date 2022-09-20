40acbafc28d0ed166defd44c433a2f2e5b61d793
Eagsor <u20075655303@gmail.com>
2022-09-19 22:18:07 -0700

    5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * print_rev -prints a string in reverse
+ * @s: the value to print to stdout
+ */
+void print_rev(char *s)
+{
+	int i, len;
+
+	len = strlen(s)
+
+	for (i = (len - 1); i > = 0; i--)
+	{
+		putchar(s[i]);
+	}
+	putchar('\n')
+}
