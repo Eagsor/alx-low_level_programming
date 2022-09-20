36a3077ea34c5d194908a8e3786de38681f9a68a
Eagsor <u20075655303@gmail.com>
2022-09-19 18:59:23 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strlen - Entry, count string function
+ *
+ *@s: This is the parameter/ arguement to be counted
+ *
+ * Return: int on success
+ */
+
+int _strlen(char *s);
+{
+	int count = 0;
+
+	While(s[count])
+	{
+		count++;
+	}
+	return (count);
+}
