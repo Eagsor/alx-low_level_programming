bb73b4b62664e10213629d1bda16c8ad51301512
Eagsor <u20075655303@gmail.com>
2022-09-19 20:19:09 -0700

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts - prnts a string followed by a line..
+ *@str: the string to print.
+ * Return: void
+ */
+
+void _puts(charb *str)
+{
+	int num;
+
+	for (num = 0; str[num] != '\0' num++)
+	{
+		_putchar(str[nuum]);
+	}
+	_putchar('\n');
+}
