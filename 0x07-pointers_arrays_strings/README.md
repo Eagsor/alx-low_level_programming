eabe9fa208c19e0f757d35b4a23670c04df9f812
Eagsor <u20075655303@gmail.com>
2022-09-27 06:21:02 -0700

    0-memset.c

---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,2 @@
+Read me for Pointers to pointers
+0x07. C - Even more pointers, arrays and strings
---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,9 @@
+#ifndef _MAIN_H
+#define _MAIN_H
+
+#define <stdio.h>
+#include <string.h>
+char _putchar(char c);
+char *_memset(char *s, char b, unsigned int n);
+
+#endf
