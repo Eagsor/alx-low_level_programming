b33964047af4c23753239d07f16d449fd910d1d1
Eagsor <u20075655303@gmail.com>
2022-10-01 18:36:42 -0700

    1-memcpy.c

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,25 @@
+# define "main.h"
+
+/**
+ * _memcpy() function copies n bytes
+ * from memory area src to memory area dest
+ *
+ * @dest: The destination address
+ * @src: The address of memory to print
+ * @n: The size of the memory to print
+ *
+ * Returns: A pointer to dest
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int count;
+	char *dest = dest;
+	char const *Psrc = src;
+
+	for (count = 0; count <= n; count++)
+	{
+		*dest++ = *Psrc++;
+	}
+	return (dest);
+}
