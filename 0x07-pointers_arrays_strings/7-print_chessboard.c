61ca141ef4841444189b5ced754c0b13b9d20ecc
Eagsor <u20075655303@gmail.com>
2022-10-04 03:16:24 -0700

    7-print_chessboard.c

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,6 +1,5 @@
 #include "main.h"
 #include <stdio.h>
-
 /**
  * print_chessboard - Write a function that prints the chessboard.
  *
@@ -20,4 +19,4 @@ void print_chessboard(char (*a)[8])
 		}
 		_putchar('\n');
 	}
-{
+}
